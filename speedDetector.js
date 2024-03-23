2b17f2d08bca43467486723678a8ce1300cba73a
+ <+>
2024-03-23 20:14:06 +0300

    Code Working as per required

---
+++ speedDetector.js
@@ -0,0 +1,34 @@
+const prompt = require("prompt-sync")({ sigint: true }); 
+
+// Define our function that works out our speed and demerit points
+function calculateDemeritPoits(speed) {
+      // Ensure the value for speed is not null/empty
+    if (speed === "") {
+        return "Invalid input. Enter a valid numerical value for speed";
+    }
+       
+    const speedLimit = 70
+    let demeritPoints=0
+
+    if (speed > speedLimit){
+        const extraSpeed=(speed-speedLimit)
+        demeritPoints = Math.floor(extraSpeed / 5); // Calculate demerit points and use Math.floor to round our result  down
+    }
+      // Create the output message based on speed user inputed
+
+    let message;
+    if (speed <= speedLimit) {
+      message = 'Ok';
+    } else if (demeritPoints<=12){
+        message = `Points: ${demeritPoints}`;
+    } else {
+        message="Lisence suspended"
+    }
+    return message; // Return the message
+}
+const speed = prompt("Enter Vehicle Speed: "); // Get user input
+
+console.log(calculateDemeritPoits)
+
+
+  
